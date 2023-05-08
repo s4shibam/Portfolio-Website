a6c3c56c30eb244f1238321cd373e3a30d0332b5
Shibam Saha <shibamsaha00@gmail.com>
2023-05-08 23:42:00 +0530

    summarised about details

--- src/constants/AboutData.js
+++ src/constants/AboutData.js
@@ -1,9 +1,7 @@
 const AboutData = [
-  'Shibam Saha, a 21-year-old B. Tech Computer Science student in West Bengal, India, interested in programming, problem-solving, and software development. He is a perfectionist who values collaboration as a team player and works together to achieve a shared objective.',
+  'Hi! I am Shibam Saha, a 22-year-old B. Tech Computer Science student in West Bengal, India, interested in programming, problem-solving, and software development. I aim to be a perfectionist who values collaboration as a team player and working together to achieve a shared objective.',
 
-  'Aside from his passion for technology, he have various other interests and hobbies, like, playing cricket, carrom, online games, etc. Playing these games not only keeps him active, but also helps him to build leadership, teamwork, and planning skills where as it also helps him enhance his analytical and decision-making skills.',
-
-  'He likes to watch videos on business case studies and geopolitics in free-time. These videos help to provide a unique viewpoint on the world, governments and nations. Geopolitical analysis helps him comprehend the political atmosphere and how it affects various parts of the world.',
+  'I have various interests and hobbies, such as playing cricket, carrom, and online games, which helps me to build leadership, teamwork, and planning skills. I also like to watch videos on business case studies and geopolitics to gain a unique perspective on the world.',
 
   "I'm sure that my love of technology, my other interests, and my soft skills will help me succeed in the career I've selected. I appreciate you viewing my portfolio, and I sincerely hope you find my work and experiences interesting."
 ];
